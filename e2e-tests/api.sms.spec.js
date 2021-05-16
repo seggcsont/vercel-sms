1856931fda328a2402f7019ea852ab6ab6b4e073
Laszlo Tarcsanyi <tarcsanyi.laszlo@gmail.com>
2021-05-17 00:32:47 +0200

    Add local env live env for test

--- e2e-tests/api.sms.spec.js
+++ e2e-tests/api.sms.spec.js
@@ -1,10 +1,15 @@
 const sa = require("superagent");
 
 describe("api/sms", () => {
+  const apiUrl =
+    process.env.ENVIRONMENT == "local"
+      ? "http://localhost:3000/api/sms"
+      : "http://vercel-sms.vercel.app/api/sms";
+  console.log(`Testing on ${apiUrl}`);
   it("should return bad request if sms is not a valid format", async () => {
     try {
       await sa
-        .post("http://localhost:3000/api/sms")
+        .post(apiUrl)
         .set("Content-type", "text/plain")
         .send("asdaf asdasd");
     } catch (err) {
@@ -14,7 +19,7 @@ describe("api/sms", () => {
 
   it("should return amount and place for pos transaction", async () => {
     const response = await sa
-      .post("http://localhost:3000/api/sms")
+      .post(apiUrl)
       .set("Content-type", "text/plain")
       .send("POS tranzakcio 12345Ft Hely: dummy place");
     expect(response.body.place).toBe("dummy place");
@@ -23,7 +28,7 @@ describe("api/sms", () => {
 
   it("should return amount and place for transfer", async () => {
     const response = await sa
-      .post("http://localhost:3000/api/sms")
+      .post(apiUrl)
       .set("Content-type", "text/plain")
       .send("megbizas teljesult 112233 Ft Kedv.: dummy person");
     expect(response.body.place).toBe("dummy person");
@@ -32,7 +37,7 @@ describe("api/sms", () => {
 
   it("should return amount and place for transfer", async () => {
     const response = await sa
-      .post("http://localhost:3000/api/sms")
+      .post(apiUrl)
       .set("Content-type", "text/plain")
       .send(
         "kozuzemi megbizasa teljesult:  11 123 Ft Kedv.: dummy isp 2021.12.23"
@@ -43,7 +48,7 @@ describe("api/sms", () => {
 
   it("should Capital case upper case places", async () => {
     const response = await sa
-      .post("http://localhost:3000/api/sms")
+      .post(apiUrl)
       .set("Content-type", "text/plain")
       .send("POS tranzakcio 12345Ft Hely: DUMMY PLACE");
     expect(response.body.place).toBe("Dummy Place");
@@ -51,7 +56,7 @@ describe("api/sms", () => {
 
   it("should not touch if place not all uppercase", async () => {
     const response = await sa
-      .post("http://localhost:3000/api/sms")
+      .post(apiUrl)
       .set("Content-type", "text/plain")
       .send("POS tranzakcio 12345Ft Hely: DUmmY PLACE");
     expect(response.body.place).toBe("DUmmY PLACE");
